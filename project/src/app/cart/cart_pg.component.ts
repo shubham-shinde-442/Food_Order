dffb16c39b87eeef0ad6f705e4dfc116f5b3519f
Shubham <shindeshubham0520@gmail.com>
2023-07-16 17:47:55 +0530

    Cart Added

---
+++ project/src/app/cart/cart_pg.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
+import { FoodService } from '../services/food/food.service';
+import { Cart } from '../share/models/Cart';
+import { CartItem } from '../share/models/CartItem';
+
+@Component({
+  selector: 'app-cart-page',
+  templateUrl: './cart-page.component.html',
+  styleUrls: ['./cart-page.component.css']
+})
+export class CartPageComponent implements OnInit {
+  cart!: Cart;
+
+  constructor(private cartServicce: CartService) {
+    this.setCart();
+   }
+
+  ngOnInit(): void {
+  }
+  setCart() {
+    this.cart = this.cartServicce.getCart();
+  }
+
+  removeFromCart(cartItem: CartItem) {
+    this.cartServicce.removeFromCart(cartItem.food.id);
+    this.setCart();
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString: string) {
+    const quantity = parseInt(quantityInString);
+    this.cartServicce.changeQuantity(quantity, cartItem.food.id);
+    this.setCart();
+  }
+
+}
